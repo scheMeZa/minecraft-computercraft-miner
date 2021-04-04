387a9015d1ec06fd66b4a20ccc5504e8b5226c81
Jadon Brown <jaybeezorr@gmail.com>
2021-04-04 12:38:36 +0200

    update

--- startup.lua
+++ startup.lua
@@ -16,16 +16,22 @@ function isInventoryFull()
     return true
 end
 
-function shouldKeepItem(item)
+function isWhitelistedItem(item)
     for _, value in ipairs(keep) do
         if item.name == value then return true end
     end
+    return false
+end
 
+function isFuelItem(item)
     for _, value in ipairs(fuel) do
         if item.name == value then return true end
     end
-
     return false
+end
+
+function shouldKeepItem(item)
+    return isWhitelistedItem(item) or isFuelItem(item)
 end
 
 function dropItem(slotIndex)
@@ -44,8 +50,29 @@ function sortInventory()
     end
 end
 
+function shouldRefuel()
+    return turtle.getFuelLevel() < 50
+end
+
+function consumeFuel(slotIndex)
+    turtle.select(slotIndex)
+    turtle.refuel(1)
+    turtle.select(1)
+end
+
+function refuel()
+    for slotIndex = 1, 16, 1
+    do
+        local item = turtle.getItemDetail(slotIndex)
+        if item then
+            if isFuelItem(item) == true then return consumeFuel(slotIndex) end
+        end
+    end
+end
+
 while true do
     if isInventoryFull() then break end
+    if shouldRefuel() then refuel() end
 
     turtle.dig()
     sortInventory()
@@ -75,3 +102,5 @@ while true do
 
     turtle.forward()
 end
+
+print("My inventory is full! Mr. Stark, I don't feel so good...")
